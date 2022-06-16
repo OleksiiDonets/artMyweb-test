12ecffe33a2521c4e8335939aec2cf0ecb28b1e8
AleksejDonets <brut94@gmail.com>
2022-06-16 15:14:10 +0300

    remove counter

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,9 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
-import counterReducer from '../features/counter/counterSlice';
 import { usersApi } from '../api/userApi';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     [usersApi.reducerPath]: usersApi.reducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(usersApi.middleware)
